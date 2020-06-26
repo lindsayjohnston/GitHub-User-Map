8e8063bb01dc516f7574d9823f514fa0f02ee937
Lindsay Johnston <lindsaykjohnston@outlook.com>
2020-06-26 15:42:10 -0700

    App works and is somewhat stylized but is limited by GitHub authorization and GeoCode quirkiness with not always responding

--- docs/api.js
+++ docs/api.js
@@ -1,23 +1,28 @@
 
-class GitHub {
-    constructor(){
-        this.client_id= '69bf227b826ed24a51b5';
-        this.client_secret='6cd283cd0ba25f45102f927520435645baca029f';
-        // this.repos_count=5;
-        // this.repos_sort='created: asc';
-    }
+// class GitHub {
+//     constructor(){
+//         this.client_id= 'Iv1.a8d901da9a22e78e';
+//         this.client_secret='fc8d6ced4da7cb77a1fc0800e3ebe917d4f631e2';
+//         this.user_token='bb888f5334293c92dec50abf0dcd13e4db9fbdc5';
+//     } //
 
-    async getUsersInLocation(city){
-        const locationResponse= await fetch(`https://api.github.com/search/users?q=location%3A"${city}"`);
+//     // https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`
+
+//     async getUsersInLocation(city){
+//         const locationResponse= await fetch(`https://api.github.com/search/users?q=location%3A"${city}"`, {
+//             headers: new Headers ({
+//                 Authorization: `token ${this.user_token}` 
+//             })
+//         });
 
 
-        const location= await locationResponse.json();
+//         const location= await locationResponse.json();
         
-        return {
-            location
-        }
-    }
-}
+//         return {
+//             location  //location.total_count
+//         }
+//     }
+// }
 
 class GeoNames {
     constructor(){
--- docs/app.js
+++ docs/app.js
@@ -4,97 +4,84 @@ const coordinatesText = document.getElementById('coordinates-text')
 const citiesText = document.getElementById('cities-text');
 let latitude;
 let longitude;
-const cityBB = { //Hardcoded for now to be Washington state
-    north: 48,
-    south: 45.54,
-    east: -116.915,
-    west: -124.76
+const cityBB = { //Hardcoded dummy info for Washington state
+    north: 0,
+    south: 0,
+    east: 0,
+    west: 0
 }
-let citiesArray = ["Seattle", "Kennewick", "Tacoma", 'Yakima'];
+let citiesArray = [];
 const citiesLatLng = [];
-const citiesArray2=[]; //The cities get processed out of order in GeoCode
+const citiesArray2 = []; //Because the cities get processed out of order in GeoCode
 const gitHubNumbersArray = [];
-const latLngArray = [];
+// const latLngArray = []; dont think I need this
 let map;
 let service;
 let infoWindow;
+//Checks must be true for the next function to fire
+let nearbyCitiesCheck = false;
+let latLngCheck = false;
+let gitHubCheck = false;
 
+document.getElementById('get-map').addEventListener('click', getCityBBCoordinates);
 document.getElementById("get-bb").addEventListener('click', getCityBBCoordinates);
-document.getElementById('get-nearby-cities').addEventListener('click', getNearbyCitiesAndUsers);
+document.getElementById('get-nearby-cities').addEventListener('click', getNearbyCities);
 document.getElementById('get-latlng').addEventListener('click', getLatLng);
-document.getElementById('get-map').addEventListener('click', getMap);
+// document.getElementById('get-map').addEventListener('click', getMap);
+document.getElementById('get-gh-users').addEventListener('click', getGitHubUsers);
+
 
 //Change user input to Boundary Box coordinates readable by GeoNames.org
 function getCityBBCoordinates() {
-    //User input of city
-    //output coordinates text
-    
-    for(let i=0; i<states.length; i++){
-        if(placeArea.value === states[i][0]){
-            cityBB.north=states[i][5];
-            cityBB.south=states[i][3];
-            cityBB.east= states[i][4];
-            cityBB.west=states[i][2];
+    document.getElementById('coordinates-text').textContent = " ";
+    for (let i = 0; i < states.length; i++) {
+        if (placeArea.value === states[i][0]) {
+            cityBB.north = states[i][5];
+            cityBB.south = states[i][3];
+            cityBB.east = states[i][4];
+            cityBB.west = states[i][2];
             break;
-        } 
-        
+        }
     }
+    document.getElementById('coordinates-text').textContent += placeArea.value + " " + JSON.stringify(cityBB);
 
-    console.log(cityBB);
-    // for (let key in cityBB) {
-    //     coordinatesText.textContent += key + ": " + cityBB[key] + "// ";
-    // }
-
+    getNearbyCities();
 }
 
-function getNearbyCitiesAndUsers() {
+function getNearbyCities() {
+    citiesText.textContent = " ";
     const geoNames = new GeoNames;
     geoNames.getNearbyCities(cityBB)
         .then(data => {
             if (data.status !== undefined) {
                 alert('There was a problem with the GeoNames server and we will use dummy data to run the App. Sorry about that');
+                citiesArray = ["Seattle", "Kennewick", "Tacoma", 'Yakima'];
                 citiesArray.forEach(city => {
-                    placeArea.value = "Seattle";
                     citiesText.textContent += city + " //";
-                    // getGitHubUsers(city);
                 })
 
             } else {
-                citiesArray=[];
-                    
-                console.log(citiesArray);
                 data.geonames.forEach(cityInfo => {
                     citiesArray.push(cityInfo.name);
                     citiesText.textContent += cityInfo.name + " //";
-                    // getGitHubUsers(cityInfo.name)
                 });
             }
-
         });
+
+    checkNearbyCities();
 }
 
-function getGitHubUsers(cityName) {
-    const github = new GitHub;
-    let cityNameForURL = cityName;
-    //Change City Name to URL format
-    let cityNameArray = cityName.split(" ");
-    if (cityNameArray.length > 1) {
-        cityNameArray.forEach(function (word, index) {
-            if (index === 0) {
-                cityNameForURL = word;
-            } else {
-                cityNameForURL += "+" + word;
-            }
-        })
+function checkNearbyCities() {
+    if (citiesArray.length === 0) {
+        setTimeout(checkNearbyCities, 200);
+    } else {
+        nearbyCitiesCheck = true;
+        getLatLng();
     }
-    //Get number of Github users in city
-    github.getUsersInLocation(cityNameForURL)
-        .then(data => {
-            document.getElementById('users-text').innerText += cityNameForURL + ": " + data.location.total_count + "// ";
-        })
-};
+}
 
 function getLatLng() {
+    document.getElementById('latlng-text').textContent = " ";
     citiesArray.forEach(city => {
         let geocoderRequest = {
             address: city
@@ -112,9 +99,55 @@ function getLatLng() {
         })
 
     })
+    checkLatLng();
+}
 
-    // getMap(citiesLatLng[0], citiesArray);
+function checkLatLng() {
+    if (citiesLatLng.length === 0) {
+        setTimeout(checkLatLng, 200);
+    } else {
+        latLngCheck = true;
+        getGitHubUsers();
+    }
+}
 
+function getGitHubUsers() { 
+    //COMMENTED OUT CODE BC OF GITHUB ERRORS
+    
+    // const github = new GitHub;
+    // citiesArray2.forEach(city => {
+    //     let cityNameForURL = city;
+    //     //Change City Name to URL format
+    //     let cityNameArray = city.split(" ");
+    //     if (cityNameArray.length > 1) {
+    //         cityNameArray.forEach(function (word, index) {
+    //             if (index === 0) {
+    //                 cityNameForURL = word;
+    //             } else {
+    //                 cityNameForURL += "+" + word;
+    //             }
+    //         })
+    //     }
+    //     github.getUsersInLocation(cityNameForURL)
+    //         .then(data => {
+    //             gitHubNumbersArray.push([city, data.location.total_count]);
+    //             document.getElementById('users-text').innerText += city + ": " + data.location.total_count + "// ";
+    //         })
+
+    // })
+    citiesArray.forEach((city)=>{
+        gitHubNumbersArray.push([city, 'DummyNumber']);
+    })
+    checkGitHub();
+};
+
+function checkGitHub() {
+    if (gitHubNumbersArray.length !== citiesLatLng.length) {
+        setTimeout(checkGitHub, 200);
+    } else {
+        gitHubCheck = true;
+        getMap();
+    }
 }
 
 function getMap() {
@@ -124,29 +157,29 @@ function getMap() {
     );
 
     for (let i = 0; i < citiesLatLng.length; i++) {
-        createMarker(citiesLatLng[i], citiesArray2[i]);
+        let numberOfUsers;
+        for (let k = 0; k < gitHubNumbersArray.length; k++) {
+            if (citiesArray2[i] === gitHubNumbersArray[k][0]) {
+                numberOfUsers = gitHubNumbersArray[k][1];
+            }
+        }
+        createMarker(citiesLatLng[i], citiesArray2[i], numberOfUsers);
     }
 
 }
 
-//CREATE MAP WITH PLACES LIBRARY
 
+function createMarker(latLng, cityName, numberOfUsers) {
 
-
-
-
-
-
-function createMarker(latLng, cityName) {
-    
     let marker = new google.maps.Marker({
         map: map,
         position: latLng,
-        title: cityName
+        animation: google.maps.Animation.DROP,
+        title: `${numberOfUsers} GitHub Users in ${cityName}`
     });
 
-    marker.addListener('click', function(){
-        infoWindow=new google.maps.InfoWindow({
+    marker.addListener('click', function () {
+        infoWindow = new google.maps.InfoWindow({
             content: marker.title
         })
         infoWindow.open(map, marker);
@@ -154,15 +187,3 @@ function createMarker(latLng, cityName) {
 
 
 }
-
-// CHANGE LOCATION TO LAT/LONG
-
-
-
-
-
-
-
-
-
-
